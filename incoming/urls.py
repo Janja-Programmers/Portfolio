840be6cdf10e2f532813cfd822311ac6b2f5294b
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-10-11 15:26:48 +0300

    Set up routes

---
+++ incoming/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('about/', about, name="about"),
+    path('services/', services, name="services"),
+    path('projects/', projects, name="projects"),
+    path('blog/', blog, name="blog"),
+    path('contact', contact, name="contact"),
+]
--- incoming/views.py
+++ incoming/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
