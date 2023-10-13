9aa82c300372806d7e425de203fc25b3ba27da9b
unknown <georgesroberto21@gmail.com>
2023-10-13 09:46:19 +0300

    Updated page routing

--- incoming/urls.py
+++ incoming/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('services/', services, name="services"),
     path('projects/', projects, name="projects"),
     path('blog/', blog, name="blog"),
+    path('team/', team, name="team"),
+    path('testimonial/', testimonial, name="testimonial"),
     path('contact', contact, name="contact"),
+    path('blankpage/', blankpage, name="blankpage"),
+    
 ]
