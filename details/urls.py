4465ef5fb506e25980eeb69b3026b2807294c55e
unknown <georgesroberto21@gmail.com>
2023-10-22 21:54:35 +0300

    Details urls

--- details/urls.py
+++ details/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from .views import *
 
 #Registration of urls
-urlpatterns = []
+urlpatterns = [
+    path('', create_project, name="home"),
+    path('list/', list_project, name="list"),
+    path('update/<int:pk>/', update_project, name="update"),
+    path('delete/<int:pk>/', delete_project, name="delete"),
+]
