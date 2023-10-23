b8fb73cda790ea30159e3d6d82307c9a9bbf9868
unknown <georgesroberto21@gmail.com>
2023-10-23 14:10:30 +0300

    My urls

--- incoming/urls.py
+++ incoming/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import *
+from blog import views 
 
 urlpatterns = [
     path('', home, name="home"),
     path('about/', about, name="about"),
     path('projects/', projects, name="projects"),
-    path('blog/', blog, name="blog"),
+    path('blog/', views.home, name="blog"),
     path('team/', team, name="team"),
     path('contact', contact, name="contact"),
     path('blankpage/', blankpage, name="blankpage"),
