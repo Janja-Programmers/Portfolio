1fe705858ab1fb89255f8cb0fe09f9fedbbcc16c
unknown <georgesroberto21@gmail.com>
2023-10-23 10:50:00 +0300

    Edited Home url

--- details/urls.py
+++ details/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 #Registration of urls
 urlpatterns = [
-    path('', create_project, name="home"),
+    path('create/', create_project, name="create"),
     path('list/', list_project, name="list"),
     path('update/<int:pk>/', update_project, name="update"),
     path('delete/<int:pk>/', delete_project, name="delete"),
