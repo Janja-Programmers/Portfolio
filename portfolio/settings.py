b69360dc71c70ad5f852e1d9a9a7319856353dfa
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-10 14:30:01 +0300

    added blog

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'incoming',
     'bootstrap5',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -88,12 +89,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
