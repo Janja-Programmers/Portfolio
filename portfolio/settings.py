c29d202f9502c9c6654246a0a26dfa5c25bc7e55
unknown <georgesroberto21@gmail.com>
2023-11-17 20:11:26 +0300

    Updated Email Settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '911cybersolutions@gmail.com'
-EMAIL_HOST_PASSWORD = '#1Cybercafe'
+EMAIL_HOST_USER = 'janjaprogrammers@gmail.com'
+EMAIL_HOST_PASSWORD = 'wotp hoqo hfuz sahu'
