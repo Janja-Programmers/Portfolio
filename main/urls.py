ae5ceb29dfeee208f797b127979382d25abb27b3
Kalutu <kalutudaniel@gmail.com>
2024-11-02 18:38:30 +0300

    Contact Page

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('services/', views.services, name='services'),
-    path('projects/', views.projects, name='projects'),
+    path('enroll/', views.learn, name='learn'),
     path('contact/', views.contact, name='contact'),
-    path('appointment/', views.appointment, name='appointment')
 ]
--- main/views.py
+++ main/views.py
@@ -3,17 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
-def about(request):
-    return render(request, 'about.html')
-
-def services(request):
-    return render(request, 'services.html')
-
-def projects(request):
-    return render(request, 'projects.html')
+def learn(request):
+    return render(request, 'learn.html')
 
 def contact(request):
     return render(request, 'contact.html')
-
-def appointment(request):
-    return render(request, 'apponitment.html')
