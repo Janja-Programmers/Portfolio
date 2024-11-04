9e4a5074f31d88f3d7c63be2a5db1e5a20a16e8c
Kalutu <kalutudaniel@gmail.com>
2024-11-04 22:56:06 +0300

    Add palceholder

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,17 @@
 from django import forms
 
 class PaymentForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    phone_number = forms.CharField(label='Phone Number', max_length=15)
+    name = forms.CharField(
+        label='Name', 
+        max_length=100, 
+        widget=forms.TextInput(attrs={'placeholder': 'Your Full Name'})
+    )
+    email = forms.EmailField(
+        label='Email', 
+        widget=forms.EmailInput(attrs={'placeholder': 'you@example.com'})
+    )
+    phone_number = forms.CharField(
+        label='Mpesa Number', 
+        max_length=15,
+        widget=forms.TextInput(attrs={'placeholder': '0794933942'})
+    )
