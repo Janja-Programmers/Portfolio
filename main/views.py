cd52909f2540ed94d0da17fa72faccf8b07dd31e
Kalutu <kalutudaniel@gmail.com>
2024-11-06 10:31:48 +0300

    Handle exception send mail

--- main/views.py
+++ main/views.py
@@ -205,15 +205,25 @@ def payment_callback(request):
                           "You will be added to our course WhatsApp group, where we’ll share important updates and resources. " \
                           "If you have any questions, feel free to reach out to our support team.\n\n" \
                           "Best regards,\nJanja Programmers Admin\nsupport@janjaprogrammers.com\n+254 794 933 942"
-                send_mail(
-                    subject,
-                    message,
-                    settings.DEFAULT_FROM_EMAIL,
-                    [transaction.email],
-                    fail_silently=False,
-                )
-
-                return JsonResponse({"ResultCode": 0, "ResultDesc": "Payment successful and email sent"})
+                try:
+                    result = send_mail(
+                        subject,
+                        message,
+                        settings.DEFAULT_FROM_EMAIL,
+                        [transaction.email],
+                        fail_silently=False,
+                    )
+                    if result == 1:
+                        print("Email sent successfully.")
+                    else:
+                        print(f"Failed to send email.Result: {result}.")
+                except BadHeaderError:
+                    print("Invalid header found.")
+                except SMTPException as e:
+                    print(f"An error occurred while sending the email: {e}")
+                except Exception as e:
+                    print(f"An unexpected error occurred: {e}")
+                return JsonResponse({"ResultCode": 0, "ResultDesc": "Payment successful"})
 
             else:
                 transaction.status = "Failed"
