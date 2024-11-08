b4d5d4ed339d7f11f5f56e848f4dc854cf426e6b
Kalutu <kalutudaniel@gmail.com>
2024-11-08 23:37:45 +0300

    whitenoise setup

--- src/settings/production.py
+++ src/settings/production.py
@@ -30,6 +30,7 @@ CSRF_TRUSTED_ORIGINS = []
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ########################## SECURITY CONFIGURATION ############################
