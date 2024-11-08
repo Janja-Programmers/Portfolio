59064fed538db5931d25a804bd2a644445dfb7a1
Kalutu <kalutudaniel@gmail.com>
2024-11-08 22:47:10 +0300

    remove db in base setting

--- src/settings/base.py
+++ src/settings/base.py
@@ -65,17 +65,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'src.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
